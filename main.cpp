53b746101fc68ed8595d95f554ac5dabf849f6e6
Zeonix <jcjones170@gmail.com>
2023-10-16 19:51:29 +0800

    Updated File

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
+double function(double num, double pow)
+    {
+        double powered = 1;
+        if (pow == 0)
+        {
+            return 1;
+        }
+
+        else
+        {
+            for (int i = 1; i <= pow; i++)
+            {
+                powered = powered * num;
+            }
+        }
+
+        return powered;
+    }
+
 int main()
 {
-    std::cout << "Hello, World!\n";
+    double a, b;
+
+    std::cout << "Enter the base number: ";
+    std::cin >> a;
+    std::cout << "Enter the exponent number: ";
+    std::cin >> b;
+
+    std::cout << function(a, b) << std::endl;
+    
+    return 0;
 }
